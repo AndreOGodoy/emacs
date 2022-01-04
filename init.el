cf41dfe0f8baa3a2aa08b037d58dc4516b7ec9d1
AndreOGodoy <godoy.andre@hotmail.com>
2022-01-04 16:32:18 -0300

    Remove binding for compilation

--- init.el
+++ init.el
@@ -46,8 +46,6 @@
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-;; Key-binding for compile
-(global-set-key "\C-c\ \C-c" 'compile)
 ;; Better QOL
 (ido-mode t)
 (setq ido-everywhere t)
