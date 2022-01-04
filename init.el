2823ee004a89af946c8e9f6649f486b0b00e6a08
AndreOGodoy <godoy.andre@hotmail.com>
2022-01-04 14:02:59 -0300

    Make init.el automatically install packages

--- init.el
+++ init.el
@@ -2,11 +2,20 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(eval-when-compile
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
   (require 'use-package))
 
+(use-package magit
+  :defer 1
+  :ensure t)
+
+(use-package gruvbox-theme
+  :ensure t)
+
 ;; Theme
-(load-theme 'atom-one-dark t)
+(load-theme 'gruvbox-dark-medium t)
 
 ;; Font
 (set-frame-font "Iosevka-16" t)
@@ -30,7 +39,9 @@
 (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
 
 ;; Create recent files menu/key-binding
-(use-package recentf)
+(use-package recentf
+  :ensure t)
+
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
@@ -41,19 +52,26 @@
 (ido-mode t)
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
+
+(use-package smex
+  :ensure t)
 (global-set-key (kbd "M-x") 'smex)
 
 ;;;;;;;; LSP ;;;;;;;;
-(use-package eglot)
+(use-package eglot
+  :ensure t)
 (add-hook 'rust-mode-hook 'eglot-ensure)
 (add-to-list 'eglot-server-programs '(rust-mode . ("rust-analyzer")))
 
 (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
 
 (use-package pyvenv
+  :ensure t
   :config (pyvenv-mode 1))
 
 ;; Enable Company Mode
+(use-package company
+  :ensure t)
 (add-hook 'after-init-hook 'global-company-mode)
 
 ;; Don't litter my init file
